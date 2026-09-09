ebdff1e56957094edf31ea2f66b3ca994b0712c7
CLI-calculator <kaylaejembi511@gmail.com>
2026-09-09 15:25:36 +0100

    calculate_again.py

---
+++ calculate_again.py
@@ -0,0 +1,23 @@
+def add(a, b):
+    return a + b
+
+    
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+def divid(a, b):
+    return a / b
+
+def calculate_price():
+        kayla = add(10, 5)
+        op = subtract(50, 25)
+        rice = multiply(30, 3)
+        nunu = divid(100, 20)
+        final_output = kayla + op + rice + nunu
+        print(final_output)
+
+calculate_price()
