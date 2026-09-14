830cbd6d7fe7ec5ec894d9fab0cbfd1d277481e9
CLI_calculator <kaylaejembi511@gmail.com>
2026-09-14 01:01:17 +0100

    cli.py

---
+++ cli.py
@@ -0,0 +1,20 @@
+from .calculator import add
+from . calculator import subtract
+from . calculator import multiply
+from . calculator import divid
+
+num1 = float(input("enter first number->"))
+op = imput("enter operator (+, -, *, /)->")
+num2 = float(input("enter second number->"))
+
+if op = "+":
+    result = add(num1, num1)
+
+elif op = "-":
+    result = subtract(num1, num2)
+
+elif op = "*":
+    result = multiply(num1, num2)
+
+elif op = "/":
+    result = divid(nun1, num2)            
