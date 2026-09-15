a7a2f3bbb45d192c3d416c76c02b80d2c07f3790
kayla <kaylaejembi511@gmail.com>
2026-09-15 02:38:48 +0100

    Refactor calculator CLI for correct operations
    Fixed operator input handling and corrected function calls for arithmetic operations.

--- cli.py
+++ cli.py
@@ -1,20 +1,22 @@
-from .calculator import add
-from . calculator import subtract
-from . calculator import multiply
-from . calculator import divid
+from calculator_logic import add, subtract, multiply, divide
 
 num1 = float(input("enter first number->"))
-op = imput("enter operator (+, -, *, /)->")
+op = input("enter operator (+, -, *, /)->")
 num2 = float(input("enter second number->"))
 
-if op = "+":
-    result = add(num1, num1)
+if op == "+":
+    result = add(num1, num2)
 
-elif op = "-":
+elif op == "-":
     result = subtract(num1, num2)
 
-elif op = "*":
+elif op == "*":
     result = multiply(num1, num2)
 
-elif op = "/":
-    result = divid(nun1, num2)            
+elif op == "/":
+    result = divide(num1, num2)
+
+else:
+    result = "Invalid operator"
+
+print("Result:", result)
